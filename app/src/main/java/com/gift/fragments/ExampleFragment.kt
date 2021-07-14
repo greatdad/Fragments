ccc76815786c2bb85047f2e4fddead0659ad138c
greatdad <giftededoh@hotmail.com>
2021-07-14 17:37:08 +0100

    Initial commit

---
+++ app/src/main/java/com/gift/fragments/ExampleFragment.kt
@@ -0,0 +1,13 @@
+package com.gift.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ExampleFragment : Fragment(R.layout.fragment_example) {
+
+}
+
+
